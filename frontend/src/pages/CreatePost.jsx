afc4ca79aa346f09c6102c92c07d240f98e60f71
Iliad Hemmatjoo <ilyadhemmatjoo2003@gmail.com>
2024-05-21 11:51:09 -0400

    Basic layout , inputs and createPost page created and styled

---
+++ frontend/src/pages/CreatePost.jsx
@@ -0,0 +1,57 @@
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { preview } from "../assets";
+import { getRandomPrompt } from "../utils";
+import { FormField, Loader } from "../components";
+
+function CreatePost() {
+  const navigate = useNavigate();
+
+  const [form, setForm] = useState({
+    name: "",
+    prompt: "",
+    photo: "",
+  });
+
+  const [generatingImg, setGeneratingImg] = useState(false);
+  const [loading, setLoading] = useState(false);
+
+  const handleSubmit = () => {};
+  const handleChange = (e) => {};
+
+  return (
+    <section className="max-w-7xl mx-auto">
+      <div>
+        <div>
+          <h1 className="font-extrabold text-[#222328] text-[32px]">Create</h1>
+          <p className="mt-2 text-[#666e75] text-[14px] max-w-[500px]">
+            Generate an imaginative image through DALL-E AI and share it with
+            the community
+          </p>
+        </div>
+        <form className="mt-16 max-w-3xl" onSubmit={handleSubmit}>
+          <div>
+            <FormField
+              labelName="Your Name"
+              type="text"
+              name="name"
+              placeholder="Ex., john doe"
+              value={form.name}
+              handleChange={handleChange}
+            />
+            <FormField
+              labelName="Your Name"
+              type="text"
+              name="name"
+              placeholder="Ex., john doe"
+              value={form.name}
+              handleChange={handleChange}
+            />
+          </div>
+        </form>
+      </div>
+    </section>
+  );
+}
+
+export default CreatePost;

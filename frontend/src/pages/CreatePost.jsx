e6c4fb82aac66d24d805299716138689f6db2625
Iliad Hemmatjoo <ilyadhemmatjoo2003@gmail.com>
2024-05-22 10:30:25 -0400

    new url

--- frontend/src/pages/CreatePost.jsx
+++ frontend/src/pages/CreatePost.jsx
@@ -28,7 +28,7 @@ const CreatePost = () => {
     if (form.prompt) {
       try {
         setGeneratingImg(true);
-        const response = await fetch('http://localhost:8080/api/v1/dalle', {
+        const response = await fetch('https://text-to-image-ai-jz7v.onrender.com/api/v1/dalle', {
           method: 'POST',
           headers: {
             'Content-Type': 'application/json',

0c0f876dd295dad61995c02e7daff39c4c49f3fb
Iliad Hemmatjoo <ilyadhemmatjoo2003@gmail.com>
2024-05-21 23:17:25 -0400

    image generation functioanlity implemented

--- server/index.js
+++ server/index.js
@@ -1,20 +1,33 @@
 import express from 'express';
-import * as dotenv from "dotenv";
- import cors from 'cors';
+import * as dotenv from 'dotenv';
+import cors from 'cors';
 
- dotenv.config();
+import connectDB from './mongodb/connect.js';
+import postRoutes from './routes/postRoutes.js';
+import dalleRoutes from './routes/dalleRoutes.js';
 
- const app = express();
- app.use(cors());
+dotenv.config();
 
- app.use(express.json({limit:'50mb'}));
+const app = express();
+app.use(cors());
+app.use(express.json({ limit: '50mb' }));
 
- app.get('/', async (req, res) => {
-    res.send('Hello')
- })
+app.use('/api/v1/post', postRoutes);
+app.use('/api/v1/dalle', dalleRoutes);
 
- const startServer = async () => {
-    app.listen(8080, () => console.log("server started on port http://localhost:8080"))
- }
+app.get('/', async (req, res) => {
+  res.status(200).json({
+    message: 'Hello from DALL.E!',
+  });
+});
 
- startServer();
+const startServer = async () => {
+  try {
+    connectDB(process.env.MONGODB_URL);
+    app.listen(8080, () => console.log('Server started on port 8080'));
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+startServer();
---
+++ server/routes/dalleRoutes.js
@@ -0,0 +1,38 @@
+import express from 'express';
+import * as dotenv from 'dotenv';
+import {v2 as cloudinary} from 'cloudinary';
+import { OpenAI } from "openai";
+const openai = new OpenAI({ apiKey: process.env.OPENAI_API_KEY });
+
+dotenv.config();
+const router = express.Router();
+router.route('/').get((req, res) => {
+    res.send('Hello from DALL-E!')
+})
+
+
+router.route('/').post(async (req, res) => {
+    try {
+      const { prompt } = req.body;
+  
+      const aiResponse = await openai.images.generate({
+        prompt,
+        n: 1,
+        size: '1024x1024',
+        response_format: 'b64_json',
+      });
+      // console.log(aiResponse)
+  
+      const image = aiResponse.data[0].b64_json;
+      res.status(200).json({ photo: image });
+    } catch (error) {
+      console.error(error);
+    }
+  });
+
+
+
+
+export default router;
+
+

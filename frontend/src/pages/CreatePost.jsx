2bf4cf97f120befa80771476a72401bc728f3fb8
Iliad Hemmatjoo <ilyadhemmatjoo2003@gmail.com>
2024-05-23 17:10:13 -0400

    Bug fixed

--- frontend/src/pages/CreatePost.jsx
+++ frontend/src/pages/CreatePost.jsx
@@ -56,7 +56,7 @@ const CreatePost = () => {
     if (form.prompt && form.photo) {
       setLoading(true);
       try {
-        const response = await fetch('http://dalle-arbb.onrender.com/api/v1/post', {
+        const response = await fetch('https://text-to-image-ai-jz7v.onrender.com/api/v1/post', {
           method: 'POST',
           headers: {
             'Content-Type': 'application/json',

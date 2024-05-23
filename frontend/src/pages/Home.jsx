7d4369ab8a37e4fa54eb1f0d258616f0aea055e8
Iliad Hemmatjoo <ilyadhemmatjoo2003@gmail.com>
2024-05-23 17:25:56 -0400

    Fetching and rendering the created posts in home page

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -17,6 +17,35 @@ function Home() {
   const [loading, setLoading] = useState(false);
   const [allPosts, setAllPosts] = useState(null);
   const [searchText, setSearchText] = useState("");
+
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      setLoading(true);
+
+      try{
+          const response = await fetch("https://text-to-image-ai-jz7v.onrender.com/api/v1/post", {
+            method : 'GET',
+            headers:{
+              'Content-Type' : 'application/json',
+            }
+          });
+
+          if(response.ok){
+            const result = await response.json();
+            console.log(response)
+
+            setAllPosts(result.data.reverse());
+          }
+
+      }catch(error){
+          alert(error);
+      }finally{
+        setLoading(false);
+      }
+    }
+    fetchPosts();
+  }, [])
   return (
     <section>
       <div>
